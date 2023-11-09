d47bce40b44c436744e230c38ae0267503a60184
PedroGama24 <pedroaugustogama19@gmail.com>
2023-11-09 15:43:52 -0300

    Car pack V1

--- server-data/resources/[veiculos]/vrp_veiculos/fxmanifest.lua
+++ server-data/resources/[veiculos]/vrp_veiculos/fxmanifest.lua
@@ -1,15 +1,137 @@
-fx_version 'adamant'
+
+
+
+
+client_script "@vrp/lib/lib.lua" --Para remover esta pendencia de todos scripts, execute no console o comando "uninstall"
+
+fx_version 'cerulean'
 game 'gta5'
 
 files {
-	'data/**/*.meta'
-}
 
-client_script "data/tuning.lua"
+	'data/*',
+	'data/**/*',
+    'audio/*',
+	'audio/**/*',
+
+}
 
 data_file 'HANDLING_FILE' 'data/**/handling.meta'
 data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
 data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
-data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+data_file "VEHICLE_LAYOUTS_FILE" 'data/**/layouts/*.meta'
 
+
+data_file "AUDIO_WAVEPACK" "audio/elegyx/dlc_elegyx"
+data_file "AUDIO_GAMEDATA" "audio/elegyx/elegyx_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/elegyx/elegyx_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/monroe/dlc_monroec"
+data_file "AUDIO_GAMEDATA" "audio/monroe/monroec_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/monroe/monroec_sounds.dat"
+     
+data_file "AUDIO_WAVEPACK" "audio/hemisound/dlc_hemisound"
+data_file "AUDIO_GAMEDATA" "audio/hemisound/hemisound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/hemisound/hemisound_sounds.dat"
+
+data_file 'AUDIO_SYNTHDATA' 'audio/nisgtr35/nisgtr35_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/nisgtr35/nisgtr35_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/nisgtr35/nisgtr35_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/nisgtr35/dlc_nisgtr35'
+
+data_file "AUDIO_WAVEPACK" "audio/r34sound/dlc_r34sound"
+data_file "AUDIO_GAMEDATA" "audio/r34sound/r34sound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/r34sound/r34sound_sounds.dat"
+
+data_file "AUDIO_SYNTHDATA" "audio/918spyeng/918spyeng_amp.dat"
+data_file "AUDIO_GAMEDATA" "audio/918spyeng/918spyeng_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/918spyeng/918spyeng_sounds.dat"
+data_file "AUDIO_WAVEPACK" "audio/918spyeng/dlc_918spyeng"
+
+data_file "AUDIO_SYNTHDATA" "audio/b58b30/b58b30_amp.dat"
+data_file "AUDIO_GAMEDATA" "audio/b58b30/b58b30_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/b58b30/b58b30_sounds.dat"
+data_file "AUDIO_WAVEPACK" "audio/b58b30/dlc_b58b30"
+
+data_file 'AUDIO_SYNTHDATA' 'audio/rotary7/rotary7_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/rotary7/rotary7_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/rotary7/rotary7_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/rotary7/dlc_rotary7'
+
+data_file 'AUDIO_GAMEDATA' 'audio/toysupmk4/toysupmk4_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/toysupmk4/toysupmk4_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/toysupmk4/dlc_toysupmk4'
+
+data_file 'AUDIO_GAMEDATA' 'audio/cummins5924v/cummins5924v_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/cummins5924v/cummins5924v_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/cummins5924v/dlc_cummins5924v'
+
+data_file 'AUDIO_GAMEDATA' 'audio/ea888/ea888_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/ea888/ea888_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/ea888/dlc_ea888'
+
+data_file "AUDIO_SYNTHDATA" "audio/bmws1krreng/bmws1krreng_amp.dat"
+data_file "AUDIO_GAMEDATA" "audio/bmws1krreng/bmws1krreng_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/bmws1krreng/bmws1krreng_sounds.dat"
+data_file "AUDIO_WAVEPACK" "audio/bmws1krreng/dlc_bmws1krreng"
+
+data_file 'AUDIO_GAMEDATA' 'audio/ta488f154/ta488f154_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/ta488f154/ta488f154_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/ta488f154/dlc_ta488f154'
+
+data_file "AUDIO_SYNTHDATA" "audio/f136/f136_amp.dat"
+data_file "AUDIO_GAMEDATA" "audio/f136/f136_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/f136/f136_sounds.dat"
+data_file "AUDIO_WAVEPACK" "audio/f136/dlc_f136"
+
+data_file "AUDIO_GAMEDATA" "audio/ta4b11/ta4b11_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/ta4b11/ta4b11_sounds.dat"
+data_file "AUDIO_WAVEPACK" "audio/ta4b11/dlc_ta4b11"
+
+data_file 'AUDIO_GAMEDATA' 'audio/v6audiea839/v6audiea839_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/v6audiea839/v6audiea839_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/v6audiea839/dlc_v6audiea839'
+
+data_file 'AUDIO_GAMEDATA' 'audio/alfa690t/alfa690t_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/alfa690t/alfa690t_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/alfa690t/dlc_alfa690t'
+
+data_file "AUDIO_GAMEDATA" "audio/k20a/k20a_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/k20a/k20a_sounds.dat"
+data_file "AUDIO_WAVEPACK" "audio/k20a/dlc_k20a"
+
+data_file "AUDIO_GAMEDATA" "audio/rb28dett/rb28dett_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/rb28dett/rb28dett_sounds.dat"
+data_file "AUDIO_WAVEPACK" "audio/rb28dett/dlc_rb28dett"
+
+data_file "AUDIO_GAMEDATA" "audio/mercm177/mercm177_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/mercm177/mercm177_sounds.dat"
+data_file "AUDIO_WAVEPACK" "audio/mercm177/dlc_mercm177"
+
+data_file "AUDIO_GAMEDATA" "audio/aq24amgm133/aq24amgm133_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/aq24amgm133/aq24amgm133_sounds.dat"
+data_file "AUDIO_WAVEPACK" "audio/aq24amgm133/dlc_aq24amgm133"
+
+data_file "AUDIO_SYNTHDATA" "audio/bnr34ffeng/bnr34ffeng_amp.dat"
+data_file "AUDIO_GAMEDATA" "audio/bnr34ffeng/bnr34ffeng_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/bnr34ffeng/bnr34ffeng_sounds.dat"
+data_file "AUDIO_WAVEPACK" "audio/bnr34ffeng/dlc_bnr34ffeng"
+
+data_file "AUDIO_SYNTHDATA" "audio/z33dkffeng/z33dkffeng_amp.dat"
+data_file "AUDIO_GAMEDATA" "audio/z33dkffeng/z33dkffeng_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/z33dkffeng/z33dkffeng_sounds.dat"
+data_file "AUDIO_WAVEPACK" "audio/z33dkffeng/dlc_z33dkffeng"
+
+data_file "AUDIO_WAVEPACK" "audio/s15sound/dlc_s15sound"
+data_file "AUDIO_GAMEDATA" "audio/s15sound/s15sound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/s15sound/s15sound_sounds.dat"
+
+data_file "AUDIO_SYNTHDATA" "audio/kc32ducavr4/kc32ducavr4_amp.dat"
+data_file "AUDIO_GAMEDATA" "audio/kc32ducavr4/kc32ducavr4_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/kc32ducavr4/kc32ducavr4_sounds.dat"
+data_file "AUDIO_WAVEPACK" "audio/kc32ducavr4/dlc_kc32ducavr4"
+
+data_file "AUDIO_WAVEPACK" "audio/lfasound/dlc_lfasound"
+data_file "AUDIO_GAMEDATA" "audio/lfasound/lfasound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/lfasound/lfasound_sounds.dat"
