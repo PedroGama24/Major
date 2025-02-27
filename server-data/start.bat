1153235536305eb8ecff7244167a46ac2bc177ff
PedroGama24 <126327616+PedroGama24@users.noreply.github.com>
2025-02-27 17:14:58 -0300

    Update star.bat

--- server-data/start.bat
+++ server-data/start.bat
@@ -5,12 +5,12 @@ echo		[-------------------------------------------------------------------------
 echo		[---             ##   Major:  discord.gg/4xnQFpHMgM   ##               ---]
 echo		[-------------------------------------------------------------------------] 
 echo.
-echo         [-------------------------  Creditos de Dev  -----------------------------]
-echo         [---                    Base: Major_City                               ---]
-echo    	[---                    Developed by: Nomadd, Guxtavo, Cabral           ---]
+echo        [-------------------------  Creditos de Dev   -----------------------------]
+echo        [---                    Base: Major_City                                ---]
+echo    	[---                    Developed by: Nomadd                            ---]
 echo		[--------------------------------------------------------------------------] 
 echo.
-echo         [--------------------------  Redes Sociais  ------------------------------]
+echo        [--------------------------  Redes Sociais   ------------------------------]
 echo    	[---                 Discord: discord.gg/4xnQFpHMgM                     ---]
 echo    	[---                 Site:                                              ---]
 echo    	[---                 Instagram: @majorcity_oficial                      ---]
